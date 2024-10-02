959b8d10d94c8444db9cafe02ece7d89eb2865a2
Kseniia <anillinnittakorall@gmail.com>
2024-10-02 21:43:03 +0300

    added part that checks if someone won

--- src/Main.java
+++ src/Main.java
@@ -27,12 +27,32 @@ public class Main {
         Player1 p1 = new Player1("P1");
         Player2 p2 = new Player2("P2");
         System.out.println("Welcome to the game!");
+        mainGame(p1, p2, sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares);
 
-        while (p1.getIsTurn() == true || p2.getIsTurn() == false) {
-            p1.move(sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares);
-            p2.move(sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares);
-        }
     }
+
+    public static void mainGame(Player1 p1, Player2 p2, Scanner sc, TicTacToeSquare a1, TicTacToeSquare a2, TicTacToeSquare a3, TicTacToeSquare b1, TicTacToeSquare b2, TicTacToeSquare b3, TicTacToeSquare c1, TicTacToeSquare c2, TicTacToeSquare c3, ArrayList<TicTacToeSquare> allSquares) {
+        if ((a1.getValue().equals(a2.getValue()) && a2.getValue().equals(a3.getValue()) && !a1.getValue().equals(" ")) //each stands for any kind of winning (in a row och in a column)
+                || (b1.getValue().equals(b2.getValue()) && b2.getValue().equals(b3.getValue()) && !b1.getValue().equals(" "))
+                || (c1.getValue().equals(c2.getValue()) && c2.getValue().equals(c3.getValue()) && !c1.getValue().equals(" "))
+                || (a1.getValue().equals(b1.getValue()) && b1.getValue().equals(c1.getValue()) && !c1.getValue().equals(" "))
+                ||(a2.getValue().equals(b2.getValue()) && b2.getValue().equals(c2.getValue()) && !c2.getValue().equals(" "))
+        || (a3.getValue().equals(b3.getValue()) && b3.getValue().equals(c3.getValue()) && !c3.getValue().equals(" "))
+        ||(a3.getValue().equals(b2.getValue()) && b2.getValue().equals(c1.getValue()) && !c1.getValue().equals(" "))
+        || (a1.getValue().equals(b2.getValue()) && b2.getValue().equals(c3.getValue()) && !c3.getValue().equals(" "))) {
+            System.out.println("We have a winner"); //find a way to chech who won
+        } else {
+            if ((p1.getIsTurn() == false && p2.getIsTurn() == true) || (p1.getIsTurn() == true && p2.getIsTurn() == false)) {
+                p2.move(sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares);
+            } else {
+                p1.move(sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares);
+            } mainGame(p1, p2, sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares);
+        }
+
+    }
+
 }
+
+
 
 
