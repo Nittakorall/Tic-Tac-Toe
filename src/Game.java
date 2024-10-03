ebabd576080a3c9d29bfc46ed3cbad4548c74017
Kseniia <anillinnittakorall@gmail.com>
2024-10-03 15:47:07 +0300

    moved methods from main, fixed picking not empty square

---
+++ src/Game.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Game {
+    public Game() {
+    }
+
+    public void game() {
+        Scanner sc = new Scanner(System.in);
+        TicTacToeSquare a1 = new TicTacToeSquare(" ", "a1");
+        TicTacToeSquare a2 = new TicTacToeSquare(" ", "a2");
+        TicTacToeSquare a3 = new TicTacToeSquare(" ", "a3");
+        TicTacToeSquare b1 = new TicTacToeSquare(" ", "b1");
+        TicTacToeSquare b2 = new TicTacToeSquare(" ", "b2");
+        TicTacToeSquare b3 = new TicTacToeSquare(" ", "b3");
+        TicTacToeSquare c1 = new TicTacToeSquare(" ", "c1");
+        TicTacToeSquare c2 = new TicTacToeSquare(" ", "c2");
+        TicTacToeSquare c3 = new TicTacToeSquare(" ", "c3");
+        ArrayList<TicTacToeSquare> allSquares = new ArrayList<>();
+        allSquares.add(a1);
+        allSquares.add(a2);
+        allSquares.add(a3);
+        allSquares.add(b1);
+        allSquares.add(b2);
+        allSquares.add(b3);
+        allSquares.add(c1);
+        allSquares.add(c2);
+        allSquares.add(c3);
+        Player1 p1 = new Player1("P1");
+        Player2 p2 = new Player2("P2");
+        System.out.println("Welcome to the game!");
+        boolean isFirstPlayer = true; // checks which player was last, need to find the winner.
+        mainGame(p1, p2, sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares, isFirstPlayer);
+    }
+
+    public static void mainGame(Player1 p1, Player2 p2, Scanner sc, TicTacToeSquare a1, TicTacToeSquare a2, TicTacToeSquare a3, TicTacToeSquare b1, TicTacToeSquare b2, TicTacToeSquare b3, TicTacToeSquare c1, TicTacToeSquare c2, TicTacToeSquare c3, ArrayList<TicTacToeSquare> allSquares, boolean isFirstPlayer) {
+        if ((a1.getValue().equals(a2.getValue()) && a2.getValue().equals(a3.getValue()) && !a1.getValue().equals(" ")) //each stands for any kind of winning (in a row och in a column)
+                || (b1.getValue().equals(b2.getValue()) && b2.getValue().equals(b3.getValue()) && !b1.getValue().equals(" "))
+                || (c1.getValue().equals(c2.getValue()) && c2.getValue().equals(c3.getValue()) && !c1.getValue().equals(" "))
+                || (a1.getValue().equals(b1.getValue()) && b1.getValue().equals(c1.getValue()) && !c1.getValue().equals(" "))
+                || (a2.getValue().equals(b2.getValue()) && b2.getValue().equals(c2.getValue()) && !c2.getValue().equals(" "))
+                || (a3.getValue().equals(b3.getValue()) && b3.getValue().equals(c3.getValue()) && !c3.getValue().equals(" "))
+                || (a3.getValue().equals(b2.getValue()) && b2.getValue().equals(c1.getValue()) && !c1.getValue().equals(" "))
+                || (a1.getValue().equals(b2.getValue()) && b2.getValue().equals(c3.getValue()) && !c3.getValue().equals(" "))) {
+            if (isFirstPlayer == true) {
+                System.out.println("First Player won!");
+            } else {
+                System.out.println("Second player won!");
+            }
+        } else if (!a1.getValue().equals(" ") && !a2.getValue().equals(" ") && !a3.getValue().equals(" ") && //no square is empty
+                !b1.getValue().equals(" ") && !b2.getValue().equals(" ") && !b3.getValue().equals(" ") &&
+                !c1.getValue().equals(" ") && !c2.getValue().equals(" ") && !c3.getValue().equals(" ")) {
+            System.out.println("No one won");
+        } else {
+            if ((p1.getIsTurn() == false && p2.getIsTurn() == true) || (p1.getIsTurn() == true && p2.getIsTurn() == false)) {
+                p2.move(sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares);
+                isFirstPlayer = false;
+            } else {
+
+                p1.move(sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares);
+                isFirstPlayer = true;
+            }
+            mainGame(p1, p2, sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares, isFirstPlayer);
+        }
+
+    }
+}
--- src/Player1.java
+++ src/Player1.java
@@ -10,24 +10,34 @@ public class Player1 extends Player implements Move {
     @Override
     public void move(Scanner sc, TicTacToeSquare a1, TicTacToeSquare a2, TicTacToeSquare a3, TicTacToeSquare b1, TicTacToeSquare b2, TicTacToeSquare b3, TicTacToeSquare c1, TicTacToeSquare c2, TicTacToeSquare c3, ArrayList<TicTacToeSquare> allSquares) {
         System.out.println(name + ", what's your next move?");
-        String answer = sc.nextLine();
-        //  boolean correctAnswerFound = false;//Needs for loop that checks if users answer fits to the game, true when right answer found
-        //    boolean firstLoop = true; // checks if player already played, if false, for loop breaks and player 2 can play.
+        loopThroughTheField(sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares);
+        isTurn =!isTurn;
+    }
 
+    void loopThroughTheField(Scanner sc, TicTacToeSquare a1, TicTacToeSquare a2, TicTacToeSquare a3, TicTacToeSquare b1, TicTacToeSquare b2, TicTacToeSquare b3, TicTacToeSquare c1, TicTacToeSquare c2, TicTacToeSquare c3, ArrayList<TicTacToeSquare> allSquares) {
+        String answer = sc.nextLine();
         for (TicTacToeSquare s : allSquares) {
             if (answer.equals(s.getPlace()) && s.getValue().equals(" ")) {
                 s.setValue(symbol);
-
-                System.out.println("   " + a1.getValue() + " | " + a2.getValue() + " | " + a3.getValue() + " ");
+                System.out.println(" " + a1.getValue() + " | " + a2.getValue() + " | " + a3.getValue() + " ");
                 System.out.println("---+---+---");
-                System.out.println("   " + b1.getValue() + " | " + b2.getValue() + " | " + b3.getValue() + " ");
+                System.out.println(" " + b1.getValue() + " | " + b2.getValue() + " | " + b3.getValue() + " ");
                 System.out.println("---+---+---");
                 System.out.println(" " + c1.getValue() + " | " + c2.getValue() + " | " + c3.getValue() + " ");
 
+            } else if (answer.equals(s.getPlace()) && !s.getValue().equals(" ")) {
+                System.out.println("Square is taken");
+                loopThroughTheField(sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares);
+            } else if (!answer.equals("a1") && !answer.equals("a2") && !answer.equals("a3")
+                    && !answer.equals("b1") && !answer.equals("b2") && !answer.equals("b3")
+                    && !answer.equals("c1") && !answer.equals("c2") && !answer.equals("c3")) {
+                System.out.println("Please pick any empty square from a1 to c1");
+                loopThroughTheField(sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares);
             }
-
         }
-        isTurn = !isTurn;
+
     }
+
 }
+
 
