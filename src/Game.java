77ee868e78819880567ce2d36e4ad028899e6aef
Kseniia <anillinnittakorall@gmail.com>
2024-10-04 16:33:05 +0300

    Minor bug fixes, made text more readable

--- src/Game.java
+++ src/Game.java
@@ -6,7 +6,7 @@ public class Game {
     public Game() {
     }
 
-    public static void game(int firstPlayerScore, int secondPlayerScore) {
+    public static void game(int firstPlayerScore, int secondPlayerScore, Player1 p1, Player2 p2) {
         Scanner sc = new Scanner(System.in);
         TicTacToeSquare a1 = new TicTacToeSquare(" ", "a1");
         TicTacToeSquare a2 = new TicTacToeSquare(" ", "a2");
@@ -27,22 +27,8 @@ public class Game {
         allSquares.add(c1);
         allSquares.add(c2);
         allSquares.add(c3);
-        System.out.println("First player, what's your name?");
-        String playerName = sc.nextLine();
-        Player1 p1 = new Player1(playerName);
-        System.out.println("Nice to meet you, " + playerName + "! Second player, what's your name?");
-        playerName = sc.nextLine();
-        Player2 p2 = new Player2(playerName);
-        if (p1.getName().equals(p2.getName())) {
-            p1.setName(p1.getName() + " (X)");
-            p2.setName(p2.getName() + " (O)");
-            System.out.println("Oh no, you have same names! First player will be " + p1.getName() + " and second player will be " + p2.getName());
-        }
-        else {
-            System.out.println("Nice to meet you too, " + p2.getName());
-        }
+
         boolean isFirstPlayer = true; // checks which player was last, need to find the winner.
-        System.out.println("Time to start!");
         mainGame(p1, p2, sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares, isFirstPlayer, firstPlayerScore, secondPlayerScore);
     }
 
@@ -56,14 +42,14 @@ public class Game {
                 || (a3.getValue().equals(b2.getValue()) && b2.getValue().equals(c1.getValue()) && !c1.getValue().equals(" "))
                 || (a1.getValue().equals(b2.getValue()) && b2.getValue().equals(c3.getValue()) && !c3.getValue().equals(" "))) {
             if (isFirstPlayer == true) {
-                System.out.println(p1.getName() + "wins!");
+                System.out.println(p1.getName() + " wins!");
                 firstPlayerScore++;
                 System.out.println("Total score is " + firstPlayerScore + ":" + secondPlayerScore);
                 playAgainQuestion(p1, p2, sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares, isFirstPlayer, firstPlayerScore, secondPlayerScore);
 
 
             } else {
-                System.out.println(p2.getName() + "wins!");
+                System.out.println(p2.getName() + " wins!");
                secondPlayerScore++;
                 System.out.println("Total score is " + firstPlayerScore + ":" + secondPlayerScore);
                 playAgainQuestion(p1, p2, sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares, isFirstPlayer, firstPlayerScore, secondPlayerScore);
@@ -71,7 +57,7 @@ public class Game {
         } else if (!a1.getValue().equals(" ") && !a2.getValue().equals(" ") && !a3.getValue().equals(" ") && //no square is empty
                 !b1.getValue().equals(" ") && !b2.getValue().equals(" ") && !b3.getValue().equals(" ") &&
                 !c1.getValue().equals(" ") && !c2.getValue().equals(" ") && !c3.getValue().equals(" ")) {
-            System.out.println("No one won");
+            System.out.println("No one wins");
             System.out.println("Total score is " + firstPlayerScore + ":" + secondPlayerScore);
             playAgainQuestion(p1, p2, sc, a1, a2, a3, b1, b2, b3, c1, c2, c3, allSquares, isFirstPlayer, firstPlayerScore, secondPlayerScore);
         } else {
@@ -97,8 +83,8 @@ public class Game {
             if (playAgainAnswer.equals("yes") || playAgainAnswer.equals("no")) {
                 validInput = true;
                 if (playAgainAnswer.equals("yes")) {
-                    System.out.println("Let's play one more time!");
-                    game(firstPlayerScore, secondPlayerScore);
+                    System.out.println("Let's play one more time! Second player starts!"); // not a bug, feature ;_;
+                    game(firstPlayerScore, secondPlayerScore, p1, p2);
                     return;
                 } else {
                     System.out.println("That was fun. Come again!");
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,33 @@ import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
+
+
+// add tolowercase!!!!
+
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //moved Player objects here to prevent name changing if the game restarts. Full restart needed to change names
      Game game = new Game();
         System.out.println("Welcome to the game!");
         int firstPlayerScore = 0;
         int secondPlayerScore = 0;
-     game.game(firstPlayerScore, secondPlayerScore);
+        System.out.println("First player, what's your name?");
+        String playerName = sc.nextLine();
+        Player1 p1 = new Player1(playerName);
+        System.out.println("Nice to meet you, " + playerName + "! Second player, what's your name?");
+        playerName = sc.nextLine();
+        Player2 p2 = new Player2(playerName);
+        if (p1.getName().equals(p2.getName())) {
+            p1.setName(p1.getName() + " (X)");
+            p2.setName(p2.getName() + " (O)");
+            System.out.println("Oh no, you have same names! First player will be " + p1.getName() + " and second player will be " + p2.getName());
+        }
+        else {
+            System.out.println("Nice to meet you too, " + p2.getName() + "! \n Let's begin!");
+        }
+     game.game(firstPlayerScore, secondPlayerScore, p1, p2);
     }
 
 }
